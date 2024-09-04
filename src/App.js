b2da3861aaecaef02011d4b37cdc0690dde9f50c
se3rik <se3rik@mail.ru>
2024-09-04 21:15:01 +0300

    feat: work with states

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { useState } from "react";
+
 function App() {
+  const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState('Input placeholder')
+
+  function increment() {
+    setLikes(likes + 1);
+  }
+
+  function decrement() {
+    setLikes(likes - 1);
+  }
+
+  function changeValue(event) {
+    setValue(event.target.value);
+  }
+
   return (
     <div className="App">
-      <h1>React-app initialization</h1>
+      <h1>Likes: {likes}</h1>
+      <h1>Input Value: {value}</h1>
+      <input value={value} onChange={changeValue} />
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+
     </div>
   );
 }
