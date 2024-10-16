59fc6df967772a1c5ff4188f5d6b152c8dd4975c
se3rik <se3rik@mail.ru>
2024-10-16 16:44:50 +0300

    feat: loader component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal.tsx";
 import MyButton from "./components/UI/button/MyButton.jsx";
+import MyLoader from "./components/UI/loader/MyLoader.jsx";
 
 import "./styles/App.css";
 import PostService from "./api/PostService.js";
@@ -15,6 +16,7 @@ export default function App() {
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+  const [isPostsLoading, setIsPostsLoading] = useState(false);
 
   function createPost(newPost) {
     setPosts([...posts, newPost]);
@@ -26,13 +28,15 @@ export default function App() {
   }
 
   async function fetchPosts() {
+    setIsPostsLoading(true);
     const posts = await PostService.getAll();
     setPosts(posts);
+    setIsPostsLoading(false);
   }
 
   useEffect(() => {
     fetchPosts();
-  }, []); 
+  }, []);
 
   return (
     <div className="App">
@@ -43,11 +47,23 @@ export default function App() {
         <PostForm create={createPost} />
       </MyModal>
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList
-        remove={removePost}
-        listTitle="Список постов"
-        posts={sortedAndSearchedPosts}
-      />
+      {isPostsLoading ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "50px",
+          }}
+        >
+          <MyLoader />
+        </div>
+      ) : (
+        <PostList
+          remove={removePost}
+          listTitle="Список постов"
+          posts={sortedAndSearchedPosts}
+        />
+      )}
     </div>
   );
 }
---
+++ src/components/UI/loader/MyLoader.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./MyLoader.module.css";
+
+export default function MyLoader() {
+  return (
+    <div className={classes.myLoader}></div>
+  ) 
+}
